74d31d565776b61f678f2af7256831b7b7756d3d
Anushka <at0665@srmist.edu.in>
2026-02-17 13:53:09 +0530

    uc2 soln

---
+++ Anushka.java
@@ -0,0 +1,6 @@
+public class Anushka {
+    public static void main(String[] args)
+    {
+        System.out.print("*\n**\n***\n****\n*****");
+    }
+}
--- OOPSbannerapp.java
+++ OOPSbannerapp.java
@@ -1,5 +1,5 @@
 public class OOPSbannerapp{
 public static void main(String args[]){
-System.out.println("OOPS");
+System.out.println("OOPsS");
 }
 }
